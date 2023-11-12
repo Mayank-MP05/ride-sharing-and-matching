66eab0767106f3fdfb09fa8532f6a7ddf171ec64
Mayank-MP05 <mayank5pande@gmail.com>
2023-11-12 19:10:48 +0530

    Model || Rider class implementation with all variables and constructor added

--- ride-sharing-java/src/main/java/com/example/geektrust/models/Rider.java
+++ ride-sharing-java/src/main/java/com/example/geektrust/models/Rider.java
@@ -1,5 +1,19 @@
 package com.example.geektrust.models;
 
-public class Rider {
+import java.util.ArrayList;
 
+public class Rider {
+    private String riderId;
+
+    // Rider location parameters
+    private Integer xCord;
+    private Integer yCord;
+
+    private ArrayList<String> driverIdMatched = new ArrayList<>();
+
+    public Rider(String riderId, Integer xCord, Integer yCord) {
+        this.riderId = riderId;
+        this.xCord = xCord;
+        this.yCord = yCord;
+    }
 }
